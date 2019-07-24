2f911eaeda33d5c841fe0f9192107d84f5b54f1b
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-24 15:58:15 +1000

    Change to switch-case

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,26 +3,25 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
+    switch (action.type){
+        case ('INCREMENT'):
+           return {
             counter: state.counter + 1
-        }
+        } 
+        case ('DECREMENT'):
+            return {
+                counter: state.counter - 1
+            }
+        case ('ADD'):
+           return {
+                counter: state.counter + action.value
+        } 
+        case ('SUBTRACT'):
+           return {
+                counter: state.counter - action.value
+        } 
     }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + action.value
-        }
-    }
-    if (action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.value
-        }
-    }
+    // default state
     return state;
 }
 
